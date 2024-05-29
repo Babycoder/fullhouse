3ac2b78f2d195ae11a22c79c5ff220e33de7799a
Ghazali <aymenghazali1999@gmail.com>
2024-05-29 17:20:04 +0100

    init

---
+++ admin/pages/index.jsx
@@ -0,0 +1,13 @@
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function Home() {
+  return (
+    <div>
+      <p className='bg-red-600 w-24 text-center'>hello</p>
+      
+    </div>
+  );
+}
