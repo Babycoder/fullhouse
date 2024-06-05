09b5edcc8836a2c6e08eaeb986a392fcb8f7018c
Ghazali <aymenghazali1999@gmail.com>
2024-06-05 12:55:28 +0100

    tables

--- admin/pages/products.js
+++ admin/pages/products.js
@@ -1,10 +1,35 @@
 import Layout from '@/components/Layout';
+import axios from 'axios';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 export default function Products() {
-  return( 
-  <Layout>
-    <Link className='bg-gray-200 p-2 rounded-lg' href="/products/new">Add New Product </Link>
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    axios.get('/api/products').then((response) => setProducts(response.data));
+  }, []);
 
-  </Layout>);
+  return (
+    <Layout>
+      <Link className="bg-gray-200 p-2 rounded-lg" href="/products/new">
+        Add New Product
+      </Link>
+      <table className='basic mt-4'>
+        <thead>
+          <tr>
+            <td>Product name</td>
+            <td></td>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product) => (
+            <tr>
+              <td>{product.title}</td>
+              <td>button</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Layout>
+  );
 }
--- admin/pages/products/new.js
+++ admin/pages/products/new.js
@@ -1,16 +1,22 @@
 import Layout from '@/components/Layout';
 import axios from 'axios';
+import { useRouter } from 'next/router';
 import { useState } from 'react';
 
 export default function NewProduct() {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
   const [price, setPrice] = useState('');
+  const [goToProducts, setGoToProducts] = useState(false);
+  const router = useRouter();
+
   const createProduct = async (e) => {
     e.preventDefault();
     const data = { title, description, price };
     await axios.post('/api/products', data);
+    setGoToProducts(true);
   };
+  if (goToProducts) router.push('/products');
   return (
     <Layout>
       <form onSubmit={createProduct}>
